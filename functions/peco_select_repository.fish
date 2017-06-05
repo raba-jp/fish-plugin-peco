35075ec33b15657686aeca30c9aedb7e59d95af8
Hiroki Sakuraba <sakuraba@tech.gr.jp>
2017-06-05 16:15:40 +0900

    Fix error

--- functions/peco_select_repository.fish
+++ functions/peco_select_repository.fish
@@ -1,3 +1,10 @@
 function peco_select_repository
-	ghq list | peco --query "$LBUFFER" --select-1 | xargs ghq look
+	set -l query (commandline)
+	[ -n $query ]; and set query_str "--query $query --select-1"
+	ghq list | peco $query_str | read line
+	if [ $line ]
+		ghq root | read root
+		cd $root/$line
+	commandline -f repaint
+	end
 end
