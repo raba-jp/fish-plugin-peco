13b8abc1438d266ed134fe1d3a72ce3796fae83f
Hiroki Sakuraba <sakuraba@tech.gr.jp>
2017-10-15 17:36:48 +0900

    Rename and modified scripts

--- functions/peco_process_kill.fish
+++
@@ -1,3 +0,0 @@
-function peco_process_kill
-	ps axo pid,rss,time,command | peco --query "$LBUFFER" | awk '{print $1}' | xargs kill
-end
--- functions/peco_select_history.fish
+++
@@ -1,7 +0,0 @@
-function peco_select_history
-	set -l default_options "--select-1"
-	set -l peco_args "--select-1"
-	[ (count $argv) -ne 0 ]; and set -l peco_args --query "$argv"
-	history | peco $peco_args | read result
-	[ -n $result ]; and commandline $result; or commandline ""
-end
